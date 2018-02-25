d00c21a07824c134b046ef49b6f2d25f90a27070
NariyukiMatsumoto <nmatsumoto4@gmail.com>
2018-02-25 19:23:58 +0900

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+    "jsx-a11y",
+    "import"
+  ],
+  "globals": {
+    "window": true,
+    "location": true,
+    "document": true,
+    "SubmissionError": true,
+    "google": true,
+    "@": true,
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "jsx-a11y/href-no-hash": [
+      0
+    ],
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
+      }
+    ],
+    "react/prefer-stateless-function": [
+      0
+    ],
+    "import/no-unresolved": [
+      0
+    ],
+    "import/extensions": [
+      0
+    ],
+    "max-len": [
+      1,
+      300,
+      2
+    ],
+    "import/prefer-default-export": [
+      0
+    ],
+    "arrow-body-style": [
+      0
+    ],
+    "react/jsx-no-bind": [
+      0
+    ],
+    "yoda": [
+      0
+    ],
+    "no-restricted-syntax": [
+      0
+    ],
+    "react/require-default-props": [
+      0
+    ],
+    "camelcase": [
+      0
+    ],
+    "no-irregular-whitespace": [
+      "error",
+      {
+        "skipTemplates": true
+      }
+    ]
+  }
+};
